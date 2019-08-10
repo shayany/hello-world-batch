4497c0fc314252874926229ae825a4f8c69fb486
Shayan <shayan.yazdanmehr@gmail.com>
2019-08-10 23:02:23 +0200

    first draft for hello world batch!

---
+++ src/main/java/com/example/batchhelloworld/BatchHelloWorldApplication.java
@@ -0,0 +1,57 @@
+package com.example.batchhelloworld;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class BatchHelloWorldApplication {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Step step() {
+        return stepBuilderFactory
+                .get("step1")
+                .tasklet(helloWorldTasklet())
+                .build();
+    }
+
+    private Tasklet helloWorldTasklet() {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("Hello, world!");
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
+
+    @Bean
+    public Job job() {
+        return this.jobBuilderFactory
+                .get("job")
+                .start(step())
+                .build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchHelloWorldApplication.class, args);
+    }
+
+}
