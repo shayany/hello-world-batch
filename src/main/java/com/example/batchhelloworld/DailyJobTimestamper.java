c037e46c1327eefe2af9ce5d875c2bb04e8c777e
Shayan <shayan.yazdanmehr@gmail.com>
2019-08-11 13:32:25 +0200

    Add timestamp as an incrementer to the job

---
+++ src/main/java/com/example/batchhelloworld/DailyJobTimestamper.java
@@ -0,0 +1,16 @@
+package com.example.batchhelloworld;
+
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersIncrementer;
+
+import java.util.Date;
+
+public class DailyJobTimestamper implements JobParametersIncrementer {
+    @Override
+    public JobParameters getNext(JobParameters jobParameters) {
+        return new JobParametersBuilder(jobParameters)
+                .addDate("currentDate",new Date())
+                .toJobParameters();
+    }
+}
