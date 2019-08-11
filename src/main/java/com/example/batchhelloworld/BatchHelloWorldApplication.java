f7f0093f32ee89fe67a0c2569b9876a8bf14903a
Shayan <shayan.yazdanmehr@gmail.com>
2019-08-11 11:13:55 +0200

    Using job parameters by using @Value annotation

--- src/main/java/com/example/batchhelloworld/BatchHelloWorldApplication.java
+++ src/main/java/com/example/batchhelloworld/BatchHelloWorldApplication.java
@@ -2,14 +2,14 @@ package com.example.batchhelloworld;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -28,21 +28,16 @@ public class BatchHelloWorldApplication {
     public Step step() {
         return stepBuilderFactory
                 .get("step1")
-                .tasklet(helloWorldTasklet())
+                .tasklet(helloWorldTasklet(null))
                 .build();
     }
 
-    private Tasklet helloWorldTasklet() {
-        return new Tasklet() {
-            @Override
-            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                String name = (String) chunkContext
-                        .getStepContext()
-                        .getJobParameters()
-                        .get("name");
-                System.out.println(String.format("Hello, %s!",name));
-                return RepeatStatus.FINISHED;
-            }
+    @StepScope
+    @Bean
+    public Tasklet helloWorldTasklet(@Value("#{jobParameters['name']}") String name) {
+        return (stepContribution, chunkContext) -> {
+            System.out.println(String.format("Hello, %s!",  name));
+            return RepeatStatus.FINISHED;
         };
     }
 
